e14c3dfe6c9132ac335b7f2258dbeb7e0b738be5
Danielle Lyle <karendlyle@gmail.com>
2022-06-13 18:04:35 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Container } from '@mui/system';
+import { Experimental_CssVarsProvider as CssVarsProvider, experimental_extendTheme } from '@mui/material/styles';
+
 import Wrapper from './components/Wrapper';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -6,10 +8,49 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 function App() {
+	const theme = experimental_extendTheme({
+		colorSchemes: {
+			light: {
+				palette: {
+					primary: {
+						main: '#E63946'
+					},
+					secondary: {
+						main: '#457B9D'
+					},
+					secondaryLight: {
+						main: '#A8DADC'
+					},
+					secondaryDark: {
+						main: '#1D3557'
+					},
+					light: {
+						main: '#F1FAEE'
+					},
+					text: {
+						primary: '#F1FAEE'
+					}
+				}
+			},
+			dark: {
+				palette: {
+					primary: {
+						main: '#E63946'
+					},
+					secondary: {
+						main: '#457B9D'
+					}
+				}
+			}
+		}
+	});
+
 	return (
-		<Container sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
-			<Wrapper />
-		</Container>
+		<CssVarsProvider theme={theme}>
+			<Container sx={{ height: '100vh', width: '100vw', padding: '1rem', bgcolor: '#cfe8fc' }} maxWidth={false}>
+				<Wrapper />
+			</Container>
+		</CssVarsProvider>
 	);
 }
 
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,24 +1,44 @@
 import { Grid } from '@mui/material';
+import { useState } from 'react';
 import Display from './Display';
 import Num from './Num';
+import Operator from './Operators';
 
 const Wrapper = () => {
+	const [ display, setDisplay ] = useState('0000');
+
 	return (
-		<Grid height="100%" container spacing={1}>
-			<Display />
-			<Num val="7" />
-			<Num val="8" />
-			<Num val="9" />
-			<Num val="--" />
-			<Num val="4" />
-			<Num val="5" />
-			<Num val="6" />
-			<Num val="--" />
-			<Num val="1" />
-			<Num val="2" />
-			<Num val="3" />
-			<Num val="--" />
-			<Num val="0" cols="2" />
+		<Grid
+			height="100%"
+			width="100%"
+			container
+			spacing={1}
+			sx={{
+				margin: '0'
+			}}
+		>
+			<Display display={display} />
+			<Operator op="C" cols="2" type="clear" color="secondaryDark" setDisplay={setDisplay} display={display} />
+			<Operator op="/" type="divide" setDisplay={setDisplay} display={display} />
+			<Operator op="*" type="multiply" setDisplay={setDisplay} display={display} />
+
+			<Num val="7" number="seven" setDisplay={setDisplay} display={display} />
+			<Num val="8" number="eight" setDisplay={setDisplay} display={display} />
+			<Num val="9" number="nine" setDisplay={setDisplay} display={display} />
+			<Operator op="-" type="subtract" setDisplay={setDisplay} display={display} />
+
+			<Num val="4" number="four" setDisplay={setDisplay} display={display} />
+			<Num val="5" number="five" setDisplay={setDisplay} display={display} />
+			<Num val="6" number="six" setDisplay={setDisplay} display={display} />
+			<Operator op="+" type="add" setDisplay={setDisplay} display={display} />
+
+			<Num val="1" number="one" setDisplay={setDisplay} display={display} />
+			<Num val="2" number="two" setDisplay={setDisplay} display={display} />
+			<Num val="3" number="three" setDisplay={setDisplay} display={display} />
+			<Operator op="." type="decimal" setDisplay={setDisplay} display={display} />
+
+			<Num val="0" number="zero" cols="2" color="secondaryDark" setDisplay={setDisplay} display={display} />
+			<Operator op="=" cols="2" type="equals" color="secondaryDark" setDisplay={setDisplay} display={display} />
 		</Grid>
 	);
 };
